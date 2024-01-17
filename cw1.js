fee001c4cf144eefdd99b8069dc161644ffb52f8
kirans2255 <kiransunilkumar2017@gmail.com>
2024-01-17 05:43:38 -0800

    cw3

--- cw1.js
+++ cw1.js
@@ -48,18 +48,158 @@
 // Functional Addition
 
 
-function add(n) {
-  return function(x) {
-    return n + x;
-  }
-}
+// function add(n) {
+//   return function(x) {
+//     return n + x;
+//   }
+// }
 
 
-var addOne = add(1);
-console.log(addOne(3)); 
+// var addOne = add(1);
+// console.log(addOne(3)); 
 
-var addThree = add(3);
-console.log(addThree(3)); 
+// var addThree = add(3);
+// console.log(addThree(3)); 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// How many are smaller than me?
+
+// function smaller(nums) {
+
+//       const results = []
+//       for(let i=0 ; i<nums.length;i++){
+//         let count =0
+
+//           for(let j=i+1 ;j<nums.length;j++){
+//             if(nums[j]<nums[i]){
+//               count++;
+//             }
+//           }
+//       results.push(count);
+//       }
+//       return results;
+//   }
+
+//   const int = [1,2,0]
+//   const out = smaller(int)
+//   console.log(out); 
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Rock Paper Scissors
+
+// const rps = (p1, p2) => {
+//     if(p1===p2){
+//         return "Draw!";
+//     }else if(
+//         (p1==="rock" && p2==="scissors")||
+//         (p1==="scissors" &&  p2==="paper")||
+//         (p1 === "paper" && p2 === "rock")
+//     ){
+//         return "Player 1 won!"
+//     }else{
+//         return "Player 2 won!"
+//     }
+// };
+
+// console.log(rps("paper","scissors"));
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function latestClock(a, b, c, d) {
+//     const digits = [a, b, c, d];
+//     const permutations = getPermutations(digits);
+
+//     let latestTime = "00:00";
+
+//     permutations.forEach((perm) => {
+//         const time = perm.slice(0, 2) + ":" + perm.slice(2);
+//         if (isValidTime(time) && time > latestTime) {
+//             latestTime = time;
+//         }
+//     });
+
+//     return latestTime;
+// }
+
+// function getPermutations(digits) {
+//     const result = [];
+
+//     function permute(arr, start) {
+//         if (start === arr.length - 1) {
+//             result.push([...arr]);
+//             return;
+//         }
+
+//         for (let i = start; i < arr.length; i++) {
+//             [arr[start], arr[i]] = [arr[i], arr[start]];
+//             permute(arr, start + 1);
+//             [arr[start], arr[i]] = [arr[i], arr[start]]; 
+//         }
+//     }
+
+//     permute(digits, 0);
+//     return result.map((perm) => perm.join(""));
+// }
+
+// function isValidTime(time) {
+//     const [hours, minutes] = time.split(":");
+//     return hours >= "00" && hours <= "23" && minutes >= "00" && minutes <= "59";
+// }
+
+// console.log(latestClock(1, 9, 8, 3)); 
+// console.log(latestClock(9, 1, 2, 5)); 
+
+
+
+
+/////Another method
+
+
+function latestClock(a, b, c, d) {
+    const times = [
+      `${a}${b}:${c}${d}`,
+      `${a}${b}:${d}${c}`,
+      `${a}${c}:${b}${d}`,
+      `${a}${c}:${d}${b}`,
+      `${a}${d}:${b}${c}`,
+      `${a}${d}:${c}${b}`,
+      `${b}${a}:${c}${d}`,
+      `${b}${a}:${d}${c}`,
+      `${b}${c}:${a}${d}`,
+      `${b}${c}:${d}${a}`,
+      `${b}${d}:${a}${c}`,
+      `${b}${d}:${c}${a}`,
+      `${c}${a}:${b}${d}`,
+      `${c}${a}:${d}${b}`,
+      `${c}${b}:${a}${d}`,
+      `${c}${b}:${d}${a}`,
+      `${c}${d}:${a}${b}`,
+      `${c}${d}:${b}${a}`,
+      `${d}${a}:${b}${c}`,
+      `${d}${a}:${c}${b}`,
+      `${d}${b}:${a}${c}`,
+      `${d}${b}:${c}${a}`,
+      `${d}${c}:${a}${b}`,
+      `${d}${c}:${b}${a}`,
+    ];
+    const time = times.filter(el => {
+      const test = el.split(":")
+      if (test[0] >= 24) return false
+      if (test[1] >= 60) return false
+      return true
+    })
+    time.sort((a, b) =>  (b.split(":")[0] - a.split(":")[0] || b.split(":")[1] - a.split(":")[1]))
+    return time[0]
+  }
+
+  console.log(latestClock(1, 9, 8, 3)); 
