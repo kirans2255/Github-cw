1beab936dc62ae673d9c2bc3cea91ee824a1c79e
kirans2255 <kiransunilkumar2017@gmail.com>
2024-01-29 20:27:33 +0530

    cw6

--- cw1.js
+++ cw1.js
@@ -256,18 +256,18 @@
 
 ///Count by X
 
-function countBy(x, n) {
-  let z = [];
+// function countBy(x, n) {
+//   let z = [];
 
-  for (let i = 1; i <= n; i++) {
-    z.push(x * i);
-  }
+//   for (let i = 1; i <= n; i++) {
+//     z.push(x * i);
+//   }
 
-  return z;
-}
+//   return z;
+// }
 
-console.log(countBy(1, 10)); 
-console.log(countBy(2, 5));  
+// console.log(countBy(1, 10)); 
+// console.log(countBy(2, 5));  
 
 
 
@@ -276,21 +276,21 @@ console.log(countBy(2, 5));
 
 
 
-function correctTail(body, tail) {
-  // Get the substring of the body from the end, with a length equal to the tail's length
-  const sub = body.substr(body.length - tail.length);
+// function correctTail(body, tail) {
+//   // Get the substring of the body from the end, with a length equal to the tail's length
+//   const sub = body.substr(body.length - tail.length);
 
-  // Check if the substring is equal to the tail
-  if (sub === tail) {
-      return true;
-  } else {
-      return false;
-  }
-}
+//   // Check if the substring is equal to the tail
+//   if (sub === tail) {
+//       return true;
+//   } else {
+//       return false;
+//   }
+// }
 
-// Example usage:
-const isTailCorrect = correctTail("abc", "c");
-console.log(isTailCorrect); // Output: true
+// // Example usage:
+// const isTailCorrect = correctTail("abc", "c");
+// console.log(isTailCorrect); // Output: true
 
 
 
@@ -298,22 +298,22 @@ console.log(isTailCorrect); // Output: true
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-function makeNegative(number) {
-  // Check if the number is greater than 0
-  if (number > 0) {
-      // Make the number negative
-      return -number;
-  } else {
-      // If the number is already negative or zero, return as is
-      return number;
-  }
-}
+// function makeNegative(number) {
+//   // Check if the number is greater than 0
+//   if (number > 0) {
+//       // Make the number negative
+//       return -number;
+//   } else {
+//       // If the number is already negative or zero, return as is
+//       return number;
+//   }
+// }
 
-// Examples:
-console.log(makeNegative(1));     // Output: -1
-console.log(makeNegative(-5));    // Output: -5
-console.log(makeNegative(0));     // Output: 0
-console.log(makeNegative(0.12));  // Output: -0.12
+// // Examples:
+// console.log(makeNegative(1));     // Output: -1
+// console.log(makeNegative(-5));    // Output: -5
+// console.log(makeNegative(0));     // Output: 0
+// console.log(makeNegative(0.12));  // Output: -0.12
 
 
 
@@ -323,12 +323,227 @@ console.log(makeNegative(0.12));  // Output: -0.12
 
 
 
-function nthChar(words) {
-  // Use the map function to extract the nth letter from each word
-  // and then join the resulting array of letters into a single string
-  return words.map((word, index) => word.charAt(index)).join('');
+// function nthChar(words) {
+//   // Use the map function to extract the nth letter from each word
+//   // and then join the resulting array of letters into a single string
+//   return words.map((word, index) => word.charAt(index)).join('');
+// }
+
+// // Example usage:
+// const result = nthChar(["yoda", "best", "has"]);
+// console.log(result); // Output: "yes"
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Get the mean of an array
+
+// function getAverage(marks) {
+//   const sum = marks.reduce((acc, num) => acc + num, 0);
+
+//   const average = sum / marks.length;
+
+//   return Math.floor(average);
+// }
+
+// const grades = [85, 90, 92, 88, 95];
+// const averageGrade = getAverage(grades);
+// console.log(averageGrade); 
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Even or Odd - Which is Greater?
+
+
+// function evenOrOdd(str) {
+//   const numbers = str.split('').map(Number);
+
+//   const evenSum = numbers.filter(num => num % 2 === 0).reduce((acc, num) => acc + num, 0);
+//   const oddSum = numbers.filter(num => num % 2 !== 0).reduce((acc, num) => acc + num, 0);
+
+//   if (evenSum > oddSum) {
+//       return "Even is greater than Odd";
+//   } else if (oddSum > evenSum) {
+//       return "Odd is greater than Even";
+//   } else {
+//       return "Even and Odd are the same";
+//   }
+// }
+
+// const result1 =evenOrOdd("13579");
+// console.log(result1); 
+
+// const result2 =evenOrOdd("24680");
+// console.log(result2); 
+
+// const result3 =evenOrOdd("123456");
+// console.log(result3); 
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Sum of integer combinations
+
+
+// function find(arr, n) {
+//   // Helper function to find combinations recursively
+//   function findCombinationsRec(startIndex, currentCombination, currentSum) {
+//       let count = 0;
+
+//       // Check if the current combination sums up to the target
+//       if (currentSum === n) { 
+//           count++;
+//       }
+
+//       // Continue generating combinations
+//       for (let i = startIndex; i < arr.length; i++) {
+//           // Include the current element in the combination
+//           currentCombination.push(arr[i]);
+
+//           // Recursively find combinations with the updated sum
+//           count += findCombinationsRec(i, currentCombination, currentSum + arr[i]);
+
+//           // Backtrack by removing the last element to explore other combinations
+//           currentCombination.pop();
+//       }
+
+//       return count;
+//   }
+
+//   // Start the recursive process
+//   return findCombinationsRec(0, [], 0);
+// }
+
+// // Example usage:
+// const result = find([3, 6, 9, 12], 12);
+// console.log(result); // Output: 5
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+// function wallpaper(l, w, h) {
+//     const rollWidth = 0.52;
+//     const rollLength = 10;
+//     const extraLengthPercentage = 15;
+
+//     if (l * w * h === 0) {
+//         return "zero";
+//     }
+
+//     const totalWallArea = 2 * (l + w) * h; 
+//     const totalRollArea = rollWidth * rollLength * (1 + extraLengthPercentage / 100); 
+
+//     const numberOfRolls = Math.ceil(totalWallArea / totalRollArea); 
+//     if (numberOfRolls <= 20) {
+//         return numbers[numberOfRolls];
+//     } else {
+//         return numberOfRolls.toString(); 
+//     }
+// }
+
+// const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
+
+
+// console.log(wallpaper(4.0, 3.5, 3.0)); 
+// console.log(wallpaper(0.0, 3.5, 3.0)); 
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// function factory(x) {
+//   return function(array) {
+//       return array.map(function(element) {
+//           return element * x;
+//       });
+//   };
+// }
+
+// var fives = factory(5);    
+// var myArray = [1, 2, 3];
+// var result = fives(myArray);
+
+// console.log(result);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Convert a Number to a String!
+
+
+// function numberToString(num) {
+//     return String(num);
+// }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Sum of positive
+
+// function  positiveSum(arr) {
+//     const sum = arr.reduce((accumulator, currentValue) => {
+//         if (currentValue > 0) {
+//             return accumulator + currentValue;
+//         } else {
+//             return accumulator;
+//         }
+//     }, 0);
+
+//     return sum;
+// }
+
+// const result =  positiveSum([1, -4, 7, 12]);
+// console.log(result); 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// My head is at the wrong end!
+
+function fixTheMeerkat(arr) {
+    return [arr[2],arr[1],arr[0]]
 }
 
-// Example usage:
-const result = nthChar(["yoda", "best", "has"]);
-console.log(result); // Output: "yes"
+const meerkatArray = ["tail", "body", "head"];
+const fixedMeerkat = fixTheMeerkat(meerkatArray);
+console.log(fixedMeerkat); 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// Number of Rectangles in a Grid
+
+
+// function numberOfRectangles(m, n) {
+//     return (m * (m + 1) * n * (n + 1)) / 4;
+// }
+
+// console.log(countRectangles(3, 2)); 
+// console.log(countRectangles(4, 4)); 
+
+
+
